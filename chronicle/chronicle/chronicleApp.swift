b96bb2005f6854fe6a7c291ce867ffeafea28097
Aisha <aishawahid@Aishas-MacBook-Pro.local>
2024-08-25 11:03:13 +0100

    Inital Commit

---
+++ chronicle/chronicle/chronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  chronicleApp.swift
+//  chronicle
+//
+//  Created by Aisha on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct chronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
